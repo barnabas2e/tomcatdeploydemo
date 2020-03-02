bb24f92a0b62c0a9253ee074f574fd16decd7b17
Barnabas Ebadan <barnabas2e@gmail.com>
2020-03-01 21:11:01 -0800

    - Initial commit

---
+++ src/test/java/com/banny/tomcatdeploydemo/TomcatdeploydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banny.tomcatdeploydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatdeploydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
